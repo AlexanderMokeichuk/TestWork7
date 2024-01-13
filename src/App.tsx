318b2b313abcf0a4aa08b67e8bec9ac89b3ade81
AlexanderMokeichuk <alexander.q.qa.a@mail.ru>
2024-01-13 12:36:05 +0600

    Created functions for adding and deleting orders

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,63 @@
-import { useState } from 'react';
-import './App.css';
+import {useState} from "react";
+import "./App.css";
+import {amountOfOrders, Order} from "./types";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [amountOfOrders, setAmountOfOrders] = useState<amountOfOrders[]>([
+    {name: "Hamburger", count: 0},
+    {name: "Coffee", count: 0},
+    {name: "Cheeseburger", count: 0},
+    {name: "Tea", count: 0},
+    {name: "Fries", count: 0},
+    {name: "Cola", count: 0},
+  ]);
 
-  return <div></div>;
+  const orderOptions: Order[] = [
+    {name: "Hamburger", price: 80, class: ""},
+    {name: "Coffee", price: 70, class: ""},
+    {name: "Cheeseburger", price: 90, class: ""},
+    {name: "Tea", price: 50, class: ""},
+    {name: "Fries", price: 45, class: ""},
+    {name: "Cola", price: 40, class: ""},
+  ];
+
+  const changeState = (name:string) => {
+    setAmountOfOrders((prevState) => prevState.map((items) => {
+      if(items.name === name) {
+        return {
+          ...items,
+          count: items.count + 1,
+        }
+      }
+
+      return items
+    }))
+  }
+
+  const removeOrder = (name:string) => {
+    setAmountOfOrders((prevState) => prevState.map((items) => {
+      if(items.name === name) {
+        return {
+          ...items,
+          count: items.count - 1,
+        }
+      }
+
+      return items
+    }))
+  }
+
+  return (
+    <div className={"container"}>
+      <div className={"OrderDetails"}>
+
+      </div>
+
+      <div className={"AddItems"}>
+
+      </div>
+    </div>
+  )
 
 }
 
