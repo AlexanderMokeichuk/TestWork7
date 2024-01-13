083aa5309d7436e6ccfcfb069deb0c27443594c9
AlexanderMokeichuk <alexander.q.qa.a@mail.ru>
2024-01-13 15:51:11 +0600

    created a ButtonToAddOrder component and added styles

---
+++ src/Components/ButtonToAddOrder/ButtonToAddOrder.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./ButtonToAddOrder.css";
+
+interface Props {
+  name: string,
+  price: number,
+  style: string,
+  addOrder: React.MouseEventHandler
+}
+const ButtonToAddOrder: React.FC<Props> = (
+  {
+    name,
+    price,
+    style,
+    addOrder,
+  }) => {
+  return (
+    <div onClick={addOrder} className={`card`}>
+      <div className={`${style}`}></div>
+      <div className={`aboutCard`}>
+        <h6 className={`cardHeadline`}>{name}</h6>
+        <span className={`cardPrice`}>Price: ${price} KGS</span>
+      </div>
+    </div>
+  );
+};
+
+export default ButtonToAddOrder;
